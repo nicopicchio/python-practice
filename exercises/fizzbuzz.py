fa2e7959ffe92078351a67631627135bdf7a6481
Nico Picchio <nicolapicchio@gmail.com>
2022-06-20 14:22:08 +0100

    started fizzbuzz

---
+++ exercises/fizzbuzz.py
@@ -0,0 +1,13 @@
+# Requirements
+# Write a function that accepts 2 numbers, lower and upper.
+
+# The function should return an array containing all the numbers from lower to upper, including lower and upper themselves - with the following exceptions:
+
+# Where a number is a multiple of three (3, 6, 9, etc.) the array should contain the string Fizz instead of the number.
+# Where a number is a multiple of five (5, 10, etc.) the array should contain Buzz instead of of the number.
+# For numbers which are multiples of both three and five (15, 30, etc.) the array should contain the string FizzBuzz instead of the number.
+
+def fizzbuzz(lower, upper):
+  print(list(range(lower, upper)))
+
+fizzbuzz(1, 5)
