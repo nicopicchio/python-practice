c8454ab78e9d390f9aa91234835cc9d524750b7f
Nico Picchio <nicolapicchio@gmail.com>
2022-06-20 11:44:28 +0100

    minor change

--- word-comparator.py
+++ word-comparator.py
@@ -4,7 +4,7 @@ first_word = input('Enter the first word: ')
 second_word = input('Enter the second word: ')
 
 if first_word == second_word:
-  print('Both words are the same, enter a different word each time')
+  print('The words are the same, enter a different word each time')
 elif len(first_word) > len(second_word):
   print(f'{first_word} is longer than {second_word}')
 elif len(second_word) > len(first_word):
