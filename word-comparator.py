086a2ea09cfb9b6486033334934e2e361f1d934a
Nico Picchio <nicolapicchio@gmail.com>
2022-06-20 11:40:43 +0100

    first exercise completed

---
+++ word-comparator.py
@@ -0,0 +1,13 @@
+# Given two user inputed words write a program that determines which word is longer
+
+first_word = input('Enter the first word: ')
+second_word = input('Enter the second word: ')
+
+if first_word == second_word:
+  print('Both words are the same, enter a different word each time')
+elif len(first_word) > len(second_word):
+  print(f'{first_word} is longer than {second_word}')
+elif len(second_word) > len(first_word):
+  print(f'{second_word} is longer than {first_word}')
+else:
+  print(f'{first_word} and {second_word} are the same length')
